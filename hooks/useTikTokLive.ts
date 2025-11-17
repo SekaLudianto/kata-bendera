870b7728cfb61286d5828732af8f47bf0e262cbc
Seka Ludianto <sekaludianto@gmail.com>
2025-11-17 19:40:02 +0700

    ganti backend

--- hooks/useTikTokLive.ts
+++ hooks/useTikTokLive.ts
@@ -3,7 +3,8 @@ import { io, Socket } from 'socket.io-client';
 import { ConnectionStatus, ChatMessage, GiftNotification, TikTokGiftEvent } from '../types';
 
 // The backend server is expected to run on localhost:8081
-const TIKTOK_LIVE_BACKEND_URL = 'http://localhost:8081';
+// const TIKTOK_LIVE_BACKEND_URL = 'http://localhost:8081';
+const TIKTOK_LIVE_BACKEND_URL = 'https://tiktok-server-production-e573.up.railway.app';
 
 // Define the shape of the chat data coming from the backend
 interface TikTokChatEvent {
